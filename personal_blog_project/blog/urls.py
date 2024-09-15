762b33f4252aa04a07f4a1eaf3b0f06ca2f42c3f
Nour Eldin <nourelden.k74@gmail.com>
2024-09-15 14:55:44 +0300

    Add View for Create a New Article

--- personal_blog_project/blog/urls.py
+++ personal_blog_project/blog/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     ArticleListView, 
     ArticelDetailView,
-    DashBoard
+    DashBoard,
+    ArticleCreateView
 )
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
     path('article/<int:pk>/', ArticelDetailView.as_view(), name='article-detail'),
-    path('dashboard/', DashBoard.as_view(), name='dashboard')
+    path('dashboard/', DashBoard.as_view(), name='dashboard'),
+    path('new/', ArticleCreateView.as_view(), name='new')
 ]
--- personal_blog_project/blog/views.py
+++ personal_blog_project/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from .models import Article
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib import messages
+from django.urls import reverse
 from django.views.generic import (
     ListView, 
     DetailView,
+    CreateView
 )
 
 
@@ -31,3 +33,24 @@ class DashBoard(UserPassesTestMixin, ListView):
                        "You do not have permission to view this page.")
         
         return redirect('home')
+
+
+class ArticleCreateView(UserPassesTestMixin, CreateView):
+    model = Article
+    fields = ['title', 'content']
+
+    def test_func(self):
+        return self.request.user.is_superuser
+    
+    def handle_no_permission(self):
+        messages.error(self.request, 
+                       "You do not have permission to view this page.")
+        
+        return redirect('home')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Article created successfully!")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('dashboard')
