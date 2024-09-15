adcb32e85c1e8af8fb8d2a48c8f229ade094a104
Nour Eldin <nourelden.k74@gmail.com>
2024-09-15 15:18:40 +0300

    Add View for Update Article

--- personal_blog_project/blog/urls.py
+++ personal_blog_project/blog/urls.py
@@ -3,12 +3,14 @@ from .views import (
     ArticleListView, 
     ArticelDetailView,
     DashBoard,
-    ArticleCreateView
+    ArticleCreateView,
+    ArticleUpdateView
 )
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
     path('article/<int:pk>/', ArticelDetailView.as_view(), name='article-detail'),
     path('dashboard/', DashBoard.as_view(), name='dashboard'),
-    path('new/', ArticleCreateView.as_view(), name='new')
+    path('article/new/', ArticleCreateView.as_view(), name='new'),
+    path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name='update'),
 ]
--- personal_blog_project/blog/views.py
+++ personal_blog_project/blog/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse
 from django.views.generic import (
     ListView, 
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 
 
@@ -50,6 +51,27 @@ class ArticleCreateView(UserPassesTestMixin, CreateView):
     
     def form_valid(self, form):
         messages.success(self.request, "Article created successfully!")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('dashboard')
+    
+
+class ArticleUpdateView(UserPassesTestMixin, UpdateView):
+    model = Article
+    fields = ['title', 'content']
+
+    def test_func(self):
+        return self.request.user.is_superuser
+    
+    def handle_no_permission(self):
+        messages.error(self.request, 
+                       "You do not have permission to view this page.")
+        
+        return redirect('home')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Article updated successfully!")
         return super().form_valid(form)
 
     def get_success_url(self):
